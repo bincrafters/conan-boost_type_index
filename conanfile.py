2d8bd89046bfe5c0bd5a33cdd47a781c7e5a1926
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:22 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostType_IndexConan(ConanFile):
     name = "Boost.Type_Index"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/type_index"
+    url = "https://github.com/bincrafters/conan-boost-type_index"
+    source_url = "https://github.com/boostorg/type_index"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level5Group/1.64.0@bincrafters/testing"
