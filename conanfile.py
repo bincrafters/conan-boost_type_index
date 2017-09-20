75cbf3fa4c07bfadaa4d63ae5e3ea5955d35d817
Jerry W <jerrywiltse@gmail.com>
2017-09-20 15:29:21 -0400

    cleanup group members

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,9 @@
-from conans import ConanFile, tools, os
+from conans import ConanFile
+
 
 class BoostType_IndexConan(ConanFile):
     name = "Boost.Type_Index"
     version = "1.65.1"
-    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-type_index"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
